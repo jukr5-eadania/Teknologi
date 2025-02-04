1a58934d18fa92de0043590a985cab9e58f0655d
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-04 10:32:12 +0100

    Delegates understood

--- Teknologi/Program.cs
+++ Teknologi/Program.cs
@@ -8,8 +8,18 @@ namespace Teknologi
 {
     internal class Program
     {
+        delegate void stringDelegate(string a, string b, string c);
+
+        static void CollectString(string a, string b, string c)
+        {
+            Console.WriteLine($"{a} {b} {c}");
+        }
+
         static void Main(string[] args)
         {
+            stringDelegate tmp = CollectString;
+            tmp("Nimi", "is", "wife");
+            Console.ReadLine();
         }
     }
 }
