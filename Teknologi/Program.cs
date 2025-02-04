0e490c126e1c7acb9335639c6ab690c15c43cc65
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-04 11:16:20 +0100

    Comparison Method

--- Teknologi/Program.cs
+++ Teknologi/Program.cs
@@ -17,9 +17,54 @@ namespace Teknologi
 
         static void Main(string[] args)
         {
-            stringDelegate tmp = CollectString;
-            tmp("Nimi", "is", "wife");
+            stringDelegate str = CollectString;
+            str("Nimi", "is", "wife");
+
+            List<int> num = new List<int>() { 4, 32, 73, 312, 58215 };
+
+            //Sorts a list from lowest to highest
+            num.Sort(new Comparison<int>(lowFirst));
+
+            foreach (int item in num)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sorts a list from highest to lowest
+            num.Sort(new Comparison<int>(highFirst));
+
+            foreach (int item in num)
+            {
+                Console.WriteLine(item);
+            }
+
             Console.ReadLine();
+        }
+
+        static int lowFirst(int a, int b)
+        {
+            if (a > b)
+            {
+                return 1;
+            }
+            else if (b > a)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        static int highFirst(int a, int b)
+        {
+            if (a < b)
+            {
+                return 1;
+            }
+            else if (b < a)
+            {
+                return -1;
+            }
+            return 0;
         }
     }
 }
