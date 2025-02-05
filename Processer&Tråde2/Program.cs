6bca94b752d49c70a4eb99b6fd28561aba869230
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-05 13:27:30 +0100

    Race condition actived

---
+++ Processer&Tråde2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Processer_Tråde2
+{
+    internal class Program
+    {
+        public static int state = 5;
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Thread t = new Thread(RunMe);
+                t.Start();
+            }
+        }
+
+        static void RunMe()
+        {
+            int i = 0;
+            while (true)
+            {
+                if (state == 5)
+                {
+                    state++;
+                    Trace.Assert(state == 6, "Race condition in loop " + i);
+                }
+                state = 5;
+                i++;
+            }
+        }
+    }
+}
