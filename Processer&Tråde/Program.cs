32d248aa57f503fd3c9cf1db71c3cfda41fc0d8e
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-05 10:58:03 +0100

    Thread task done

---
+++ Processer&Tråde/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Processer_Tråde
+{
+    internal class Program
+    {
+        public static bool running = true;
+        public static int counter;
+
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(ThreadWork);
+
+            myThread.IsBackground = true;
+
+            myThread.Start();
+
+            Console.WriteLine("Bruh1");
+            while (running)
+            {
+                if (counter >= 10)
+                {
+                    running = false;
+                }
+            }
+
+            Console.WriteLine("Press any button to stop the program");
+            Console.ReadKey();
+        }
+
+        private static void ThreadWork()
+        {
+            while (running)
+            {
+                Console.WriteLine("Bruh2");
+                counter++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
